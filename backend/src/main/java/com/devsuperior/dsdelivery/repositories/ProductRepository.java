a08b4bae84d1428a959537488830125feda85ae6
Marcos MavericK <marcos@internit.com.br>
2021-01-04 22:37:54 -0300

    GET /products

---
+++ backend/src/main/java/com/devsuperior/dsdelivery/repositories/ProductRepository.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dsdelivery.repositories;
+
+import com.devsuperior.dsdelivery.entities.Product;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findAllByOrderByNameAsc();
+
+
+}
+
+
---
+++ backend/src/main/java/com/devsuperior/dsdelivery/services/ProductService.java
@@ -0,0 +1,25 @@
+package com.devsuperior.dsdelivery.services;
+
+
+import com.devsuperior.dsdelivery.ProductDTO;
+
+import com.devsuperior.dsdelivery.entities.Product;
+import com.devsuperior.dsdelivery.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<ProductDTO> findAll(){
+        List<Product> list = repository.findAllByOrderByNameAsc();
+        return list.stream().map(x -> new ProductDTO(x)).collect(Collectors.toList());
+    }
+
+}
